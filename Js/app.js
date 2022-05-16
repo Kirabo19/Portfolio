e16e096ce20e2e9e7dfae1df8d27f173429cab15
Kirabo19 <kapojames@gmail.com>
2022-05-16 20:17:48 +0300

    Edit app.js

--- Js/app.js
+++ Js/app.js
@@ -3,6 +3,13 @@ const closeMenu = document.querySelector('.menu-cross');
 const navItems = document.querySelectorAll('.mobile-menu a');
 
 function toggleMenu() {
-   document.querySelector('.mobile-menu').classList.toggle('d-block')
-   
+   document.querySelector('.mobile-menu').classList.toggle('d-block')  
+}
+
+hamburger.addEventListener('click', toggleMenu);
+closeMenu.addEventListener('click', toggleMenu);
+
+for (let navItem of navItems) {
+ 
+  navItem.addEventListener('click', toggleMenu);
 }

e61a7b6e763ec2348ed95e256d29d944a3faefc4
Kirabo19 <kapojames@gmail.com>
2022-05-20 13:34:26 +0300

    New Updates on the app.js

--- Js/app.js
+++ Js/app.js
@@ -169,3 +169,55 @@ btn2.addEventListener('click', (e) => {
 btn3.addEventListener('click', (e) => {
   displayPopup(e);
 });
+
+// contact form validation
+function showMessage(input, message, type) {
+	const msg = input.parentNode.querySelector("error-msg");
+	msg.innerText = message;
+  
+	// update the class for the input
+	input.className = type ? "success" : "error";
+  
+	return type;
+}
+
+function showError(input, message) {
+	return showMessage(input, message, false);
+}
+
+function showSuccess(input) {
+	return showMessage(input, "", true);
+}
+
+function hasValue(input, message) {
+	if (input.value.trim() === "") {
+		return showError(input, message);
+	}
+	return showSuccess(input);
+}
+
+function validateEmail(input, requiredMsg, invalidMsg) {
+	if (!hasValue(input, requiredMsg)) {
+		return false;
+	}
+	const emailRegex = /^[a-z]+@[^\s]+\.[^\s]+$/;
+
+	const email = input.value.trim();
+	if (!emailRegex.test(email)) {
+		return showError(input, invalidMsg);
+	}
+	return true;
+}
+
+const form = document.querySelector("#contactme");
+const EMAIL_REQUIRED = "Please enter your email";
+const EMAIL_INVALID = "Please enter your email address in lower case";
+
+form.addEventListener("submit", function (event) {
+	event.preventDefault();
+	let emailValid = validateEmail(form.elements["email"], EMAIL_REQUIRED, EMAIL_INVALID);
+	
+	if (emailValid) {
+		alert("Demo only. No form was posted.");
+	}
+});
